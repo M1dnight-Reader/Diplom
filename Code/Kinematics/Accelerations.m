a773fc2aa9ba6b814ca39bfb0d1cf5821ef99320
M1dnight-Reader <ga_rodionov1@mail.ru>
2026-02-25 16:43:40 +0300

    25.02.26

--- Code/Kinematics/Accelerations.m
+++ Code/Kinematics/Accelerations.m
@@ -4,7 +4,7 @@ g = 9.81; %ускорение свободного падения, м/с^2
 
 number = 1; %номер рассчитываемого звена
 T = 1.5;              % время, с
-theta = pi;       % угол, рад (90°)
+theta = pi/2;       % угол, рад (90°)
 [alpha1, omega_max1, t1] = angular_motion_profile(T, theta, number); % Получившиеся параметры
 
 number = 2; %номер рассчитываемого звена
@@ -28,3 +28,5 @@ theta = pi;       % угол, рад (90°)
 [alpha5, omega_max5, t5] = angular_motion_profile(T, theta, number); % Получившиеся параметры
 
 alpha = [alpha1, alpha2, alpha3, alpha4, alpha5];
+omega_max = [omega_max1, omega_max2, omega_max3, omega_max4, omega_max5];
+
--- Code/Reducers/First_reducer.m
+++
@@ -1,6 +0,0 @@
-clc;
-
-load('C:\Users\User\Documents\GitHub\Diplom\Code\kinematics_data.mat');
-[result] = solved_moments;
-
-
